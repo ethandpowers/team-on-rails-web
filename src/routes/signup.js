88b4b7d7d45e0607bb42617e1eb4ef746b28ed50
Ethan <ethandpowers@gmail.com>
2022-06-06 10:01:50 -0400

    2 custom buttons

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,11 +1,13 @@
 
 import React from "react";
-import Button from "../components/button";
+import Button1 from "../components/button1";
+import Button2 from "../components/button2";
 
 function Signup() {
     return (
         <div>
-            <Button>This is a customizable button</Button>
+            <Button1>This is button 1</Button1>
+            <Button2>This is button 2</Button2>
         </div>
     );
 }
